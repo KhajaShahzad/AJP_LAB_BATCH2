4fbd6686efd03acab05fb20af7071a92585091bc
Khaja Shahzad <khajashahzad90@gmail.com>
2026-03-13 15:31:55 +0530

    new update

--- Week-7/ClassWork/models/Student.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const StudentSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    course: String
-});
-
-module.exports = mongoose.model("Student", StudentSchema);
